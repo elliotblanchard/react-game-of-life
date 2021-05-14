85aa90a12162fadeea5e26cae9a2d4f498a0c17d
Elliot Blanchard <git@elliotblanchard.com>
2021-05-14 08:59:23 -0400

    Finished tutorial

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -6,6 +6,17 @@ import produce from "immer"
 const numRows = 50
 const numCols = 50
 
+const operations = [
+  [0,1],
+  [0,-1],
+  [1,-1],
+  [-1,1],
+  [1,1],
+  [-1,-1],
+  [1,0],
+  [-1,0]
+]
+
 function App() {
   const [grid,setGrid] = useState(() => {
     const rows = []
@@ -26,13 +37,57 @@ function App() {
       return;
     }
     //simulate
-    setTimeout(runSimulation, 1000)
+    setGrid(g => {
+      return produce(g, gridCopy => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0
+            operations.forEach(([x,y]) => {
+              const newI = i + x
+              const newK = k + y
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK]
+              }
+            })
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1
+            }
+          }
+        }
+      })
+    })
+
+    setTimeout(runSimulation, 100)
   },[])
 
   console.log(grid)
   return (
     <>
-    <button>start</button>
+    <button
+      onClick={() => {
+        setRunning(!running)
+        if (!running) {
+          runningRef.current = true
+          runSimulation()
+        }
+      }}
+    >{running ? "stop" : "start"}
+    </button>
+    <button
+      onClick = {() => {
+        const rows = []
+        for (let i = 0; i < numRows; i++) {
+          rows.push(Array.from(Array(numCols), () => (Math.random() > 0.5 ? 1 : 0)))
+        }
+
+        setGrid(rows)
+      }}
+    >
+      random
+    </button>    
     <div
       style={{
         display: "grid",
